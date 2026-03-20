bee618c3a0799429bf5ec993870e1aa09d23eb91
JuanD1207 <juandiazm1@outlook.com>
2026-03-20 12:35:35 -0600

    subida de rama java

--- Ejercisios/src/Modulo_1/HolaMundo.java
+++ Ejercisios/src/Modulo_1/HolaMundo.java
@@ -1,4 +1,7 @@
+package Modulo_1;
+
 public class HolaMundo {
+    //comentario chavales
     public static void main(String[] args) {
         System.out.println("Que onda chavales, ¿Como les baila?");
     }
